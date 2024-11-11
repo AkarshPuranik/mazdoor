384347ce6a02d3b39f289a6d0d8ffaf1c3f6f7a5
AkarshPuranik <akarshpuranik090@gmail.com>
2024-11-11 22:56:58 +0530

    OTP done

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Firebase core import
 import 'package:mazdoor/home_splash.dart';
 import 'package:mazdoor/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Initialize Flutter binding
+  await Firebase.initializeApp(); // Initialize Firebase
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: SplashScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'Mazdoor',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
   }
 }
